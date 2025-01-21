4d85612d11d299449cf39d56d1136bf7a33a1aa5
ManaMoot69 <westever344@gmail.com>
2025-01-20 22:57:57 -0600

    Add some stuff to the boss
    Boss can now make a wave of bullets

--- scripts/boss/boss_slime.gd
+++ scripts/boss/boss_slime.gd
@@ -11,10 +11,10 @@ func _ready():
 		
 
 func _physics_process(_delta: float) -> void:
-	$AnimatedSprite2D.play()
+	$SpriteGroup/AnimatedSprite2D.play()
 	var direction = Vector2(sin(self.rotation), cos(self.rotation))
-	$CPUParticles2D.gravity = direction * 800
-	$CPUParticles2D.direction = direction.normalized()
+	$SpriteGroup/CPUParticles2D.gravity = direction * 800
+	$SpriteGroup/CPUParticles2D.direction = direction.normalized()
 	if is_instance_valid(player):
 		velocity = (player.global_position - global_position).normalized() * SPEED
 		look_at(player.global_position)
