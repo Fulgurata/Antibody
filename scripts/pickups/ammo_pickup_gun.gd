d927a4d0d1d3d1c55218f13a4cd2db24ba0f4888
ManaMoot69 <westever344@gmail.com>
2025-01-21 23:40:03 -0600

    added ammo pickups and sounds
    added ammo pick ups and sprites and sounds when you pick it up. and I added HIM...

--- scripts/pickups/ammo_pickup_gun.gd
+++ scripts/pickups/ammo_pickup_gun.gd
@@ -2,17 +2,23 @@ extends Area2D
 
 @export var ammo_type: String = "gun"  # Ammo type: "gun", "shotgun", or "assault_rifle"
 @export var ammo_amount: int = 7       # Ammo amount to give (e.g., magazine size)
+@onready var ammo_pickup_sound: AudioStreamPlayer2D = $ammo_pickup_sound
 
+var picked_up: bool = false
 
 func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_area_entered(area: Area2D) -> void:
 	print("Collision detected with:", area.name)
-	if area.is_in_group("player"):
-		var gun: Node2D = get_tree().get_current_scene().find_child("Player").find_child("Gun")
-		#print("Adding ammo:", ammo_type, ammo_amount)
-		gun._on_ammo_pickup(ammo_type, ammo_amount) #Call add_ammo on the gun script
-		queue_free()  # Remove the pickup
-	else:
-		print("No gun script found on", area.name)
+	if picked_up == false:
+		if area.is_in_group("player"):
+			var gun: Node2D = get_tree().get_current_scene().find_child("Player").find_child("Gun")
+			#print("Adding ammo:", ammo_type, ammo_amount)
+			gun._on_ammo_pickup(ammo_type, ammo_amount) #Call add_ammo on the gun script
+			picked_up = true
+			ammo_pickup_sound.play()
+			await ammo_pickup_sound.finished
+			queue_free()  # Remove the pickup
+		else:
+			print("No gun script found on", area.name)
