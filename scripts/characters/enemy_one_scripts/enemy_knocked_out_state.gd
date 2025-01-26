158bb8cb6cbcdf177617bf43b49f5c49105c878c
Fulgurata <Neilse1123@gmail.com>
2025-01-26 15:11:32 -0600

    fixed enemy death

--- scripts/characters/enemy_one_scripts/enemy_knocked_out_state.gd
+++ scripts/characters/enemy_one_scripts/enemy_knocked_out_state.gd
@@ -3,7 +3,7 @@ extends EnemyState
 
 func enter_state(enemy_node) -> void:
 	super(enemy_node) #call parent class method (player_state.gd class "PlayerState")
-	enemy.enemy_sprite.play("enemy_one_death")
+	enemy.enemy_sprite.play("gory_death")
 	print("enemy_one_dead")
 
 func process(_delta) -> void:
