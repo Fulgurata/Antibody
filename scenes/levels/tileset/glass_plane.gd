f5fc21eda65b1d445f1bf978da556d6900994105
ManaMoot69 <westever344@gmail.com>
2025-01-26 15:12:51 -0600

    added breakable glass panles
    added panels of glass that can break.

---
+++ scenes/levels/tileset/glass_plane.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@onready var unbroken: Sprite2D = $unbroken
+@onready var breaking: AnimatedSprite2D = $breaking
+@onready var broken: Sprite2D = $broken
+@onready var block: StaticBody2D = $StaticBody2D
+
+
+var is_broken: bool = false
+
+func _ready():
+	unbroken.visible = true
+	breaking.visible = false
+	broken.visible = false
+	
+	
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("bullet"):
+		is_broken = true
+		unbroken.visible = false
+		breaking.visible = true
+		$glass_break.play()
+		breaking.play()
+		await breaking.animation_finished
+		breaking.visible = false
+		print("hiding")
+		broken.visible = true
+		for shape in block.get_children():
+			if shape is CollisionShape2D:
+				shape.disabled = true
